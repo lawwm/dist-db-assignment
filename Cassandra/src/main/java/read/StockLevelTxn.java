cfd01ad64dfaac528dbd1c72c112b98a930ea41b
darrentde <darrent2599@gmail.com>
2023-09-18 22:38:05 +0800

    Added a few lines to transaction 5

--- Cassandra/src/main/java/read/StockLevelTxn.java
+++ Cassandra/src/main/java/read/StockLevelTxn.java
@@ -2,9 +2,14 @@ package read;
 
 import utils.Transaction;
 
+import java.util.HashSet;
+
+import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Session;
 
 public class StockLevelTxn implements Transaction {
+
+  private static final String GET_ORDER_BY_DISTRICT = "SELECT * FROM orders_by_district WHERE D_W_ID = %d AND D_ID = %d LIMIT %d";
 
   private final String warehouse_id;
   private final String district_id;
@@ -19,6 +24,16 @@ public class StockLevelTxn implements Transaction {
   }
 
   public void run(Session session) {
+    String query = String.format(GET_ORDER_BY_DISTRICT, Integer.parseInt(warehouse_id),
+        Integer.parseInt(district_id), Integer.parseInt(last_l));
+
+    ResultSet rs = session.execute(query);
+
+    Set<Integer> items_below_stock_level = new HashSet<>();
+
+    for (Row row : rs) {
+
+    }
 
   }
 }
