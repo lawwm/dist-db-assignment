e11935e61c948f8fcba923b1ce8b5da57b96f160
Vishwanth2210 <77226742+Vishwanth2210@users.noreply.github.com>
2023-09-30 05:15:11 +0800

    Implement statistics tools

--- Cassandra/src/main/java/CassandraInit.java
+++ Cassandra/src/main/java/CassandraInit.java
@@ -2,6 +2,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.OptionalDouble;
 
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Session;
@@ -13,6 +17,9 @@ import utils.TransactionBuilder;
 public class CassandraInit {
     private static final String KEYSPACE_REF = "CS4224H";
     private static final String CREATE_TEMP_ITEM_ID_INDEX_QUERY = "create index temp on customer_item_denorm (ol_i_id);";
+    private static final String TRANSACTION_STATISTICS_TEMPLATE = "Total Transactions: %d, Total Elapsed Time (s): %f, " +
+            "Transaction Throughput: %.3f, Average Latency (ms): %.3f, Median Latency (ms): %d, " +
+            "95 percentile latency (ms): %d, 99 percentile latency (ms): %d \n";
 
     private static final String INVALID_ARGUMENTS_ERROR_MESSAGE = "Arguments Invalid. Enter: Host Port";
 
@@ -74,6 +81,9 @@ public class CassandraInit {
         BufferedReader reader = new BufferedReader(new FileReader("./project_files/xact_files/0.txt"));
         TransactionBuilder builder = new TransactionBuilder();
         String line;
+        long numTransaction = 0;
+        List<Long> latencies = new ArrayList<>();
+        long startTime = System.currentTimeMillis();
         while ((line = reader.readLine()) != null) {
             // process the line
             Transaction t = builder.build(reader, line);
@@ -81,8 +91,15 @@ public class CassandraInit {
                 continue;
             }
             System.out.println(t.getClass().getName());
+            long transStartTime = System.currentTimeMillis();
             t.run(session);
+            long transEndTime = System.currentTimeMillis();
+            long currLatency = transEndTime - transStartTime;
+            latencies.add(currLatency);
+            numTransaction++;
         }
+        long endTime = System.currentTimeMillis();
+        computeStatistics(numTransaction, latencies, startTime, endTime);
     }
 
     private static void clearDB(Session session) {
@@ -129,5 +146,44 @@ public class CassandraInit {
         } catch (InterruptedException | IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void computeStatistics(long numTransaction, List<Long> latencies, long startTime, long endTime) {
+        double totalElapsedTime = (endTime - startTime) / 1000.0;
+        double transThroughput = numTransaction / totalElapsedTime;
+        OptionalDouble possibleAverageLatency = latencies.stream().mapToDouble(x -> x).average();
+        double averageLatency = 0;
+        if (possibleAverageLatency.isPresent()) {
+            averageLatency = possibleAverageLatency.getAsDouble();
+        }
+        long[] latencyComputations = computeLatencies(latencies);
+        long medianLatency = latencyComputations[0];
+        long ninetyFifthPer = latencyComputations[1];
+        long ninetyNinePer = latencyComputations[2];
+        System.out.printf(TRANSACTION_STATISTICS_TEMPLATE, numTransaction, totalElapsedTime,
+                transThroughput, averageLatency, medianLatency, ninetyFifthPer, ninetyNinePer);
+    }
+
+    /**
+     * Computes the median, 95 percentile and 99 percentile of latencies
+     * @param latencies Latencies to be used for computation
+     * @return The calculated values of the latency
+     */
+    private static long[] computeLatencies(List<Long> latencies) {
+        Collections.sort(latencies);
+        int numLatencies = latencies.size();
+        int ninetyFifthPercentileIndex = (int) Math.ceil(0.95 * numLatencies);
+        long ninetyFifthPercentile = latencies.get(ninetyFifthPercentileIndex - 1);
+        int ninetyNinePercentileIndex = (int) Math.ceil(0.99 * numLatencies);
+        long ninetyNinePercentile = latencies.get(ninetyNinePercentileIndex - 1);
+        long median = 0;
+        int middleIndex = numLatencies / 2;
+        boolean isEvenMiddleIndex = middleIndex % 2 == 0;
+        if (isEvenMiddleIndex) {
+            median = (latencies.get(middleIndex) + latencies.get(middleIndex - 1)) / 2;
+        } else {
+            median = latencies.get(middleIndex);
+        }
+        return new long[]{median, ninetyFifthPercentile, ninetyNinePercentile};
     }
 }
